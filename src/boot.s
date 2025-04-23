3ba7f4a8603528de2986ded5e9df4e4e4a18713e
Sam <104501184+LetMeGetAByte@users.noreply.github.com>
2025-04-23 19:00:10 -0400

    Added bootstrap asm stub
    Updated gitignore to hide build folder

---
+++ src/boot.s
@@ -0,0 +1,30 @@
+.set ALIGN,		1<<0
+.set MEMINFO,	1<<1
+.set FLAGS,		ALIGN | MEMINFO
+.set MAGIC,		0x1BADB002
+.set CHECKSUM,	-(MAGIC + FLAGS)
+
+.section .multiboot
+.align 4
+.long MAGIC
+.long FLAGS
+.long CHECKSUM
+
+.section .bss
+.align 16
+stack_base_ptr:
+.skip 16384
+stack_ptr:
+
+.section .text
+.global _start
+.type _start, @function
+_start:
+	mov $stack_ptr, %esp
+	call kernel_main
+	cli
+
+1:	hlt
+	jmp 1b
+
+.size _start, . - _start
